17be5f9780ccf06d05de337172565c3a76f7ed68
Xavier Perez <xperez007@gmail.com>
2020-02-12 20:48:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,1 @@
-def introduction
-  
-end
-
-def introduction(name)
-  puts " Hi, my name is #
-  {Josh}."
-  end
-  # Lib/introduction.RUBY_VERSION
-
-def  introduction
-end
-#lib/introduction.RUBY_VERSION
-
-def  introduction(name)
-end
+def 
